c6766ad36fa186e3f69b2c3a10b2da7eb210670f
Slava Bezgachev <bezgachev@gmail.com>
2018-12-25 18:27:28 +0600

    Added a very rudimentary error boundary page
    Signed-off-by: Slava Bezgachev <bezgachev@gmail.com>

--- src/components/ErrorBoundary/index.js
+++ src/components/ErrorBoundary/index.js
@@ -1,10 +1,29 @@
 import React, { Component } from "react";
+import styled from "styled-components";
 import * as Sentry from "@sentry/browser";
+
+const ErrorWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  flex-direction: column;
+
+  h1 {
+    font-size: 2.5rem;
+    margin-bottom: 1rem;
+  }
+
+  p {
+    font-size: 1.3rem;
+    margin: 0.5rem 2rem;
+  }
+`;
 
 export default class index extends Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { hasError: true };
   }
 
   static getDerivedErrorFromError(error) {
@@ -24,7 +43,25 @@ export default class index extends Component {
   render() {
     if (this.state.hasError) {
       // Render error UI
-      return <h1>Error</h1>;
+      return (
+        <ErrorWrapper>
+          <h1>Sorry, looks like we have an error</h1>
+          <p>
+            Unfortunately BE-Dice wasn't able to recover from the error. Try
+            reloading the page.
+          </p>
+          <p>
+            The developer has been automatically notified about this error, but
+            feel free to report it on{" "}
+            <a
+              href="https://github.com/slavabez/be-dice-client"
+              target="_blank"
+            >
+              GitHub
+            </a>
+          </p>
+        </ErrorWrapper>
+      );
     }
 
     return this.props.children;
