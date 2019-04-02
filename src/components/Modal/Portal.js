9b6f6b1c71a673f8596de487d265a215e16afd31
Slava Bezgachev <bezgachev@gmail.com>
2019-04-02 09:38:13 +0600

    Adding some style

---
+++ src/components/Modal/Portal.js
@@ -0,0 +1,9 @@
+import { createPortal } from "react-dom";
+import usePortal from "./usePortal";
+
+const Portal = ({ id, children }) => {
+  const target = usePortal(id);
+  return createPortal(children, target);
+};
+
+export default Portal;
--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,9 +1,26 @@
-import { createPortal } from "react-dom";
-import usePortal from "./usePortal";
+import React from "react";
+import styled from "styled-components";
 
-const Modal = ({ id, children }) => {
-  const target = usePortal(id);
-  return createPortal(children, target);
+import Portal from "./Portal";
+
+const ModalWrapper = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ModalCard = styled.div`
+  position: relative;
+  min-width: 320px;
+  z-index: 10;
+  margin-bottom: 100px;
+`;
+
+const Modal = () => {
+  return <Portal />;
 };
 
 export default Modal;
