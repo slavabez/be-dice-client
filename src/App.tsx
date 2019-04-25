cc214286a3893b7be0e43f4432b3d6463f3f4fa4
Slava Bezgachev <bezgachev@gmail.com>
2019-04-25 17:20:41 +0600

    Added styled Themes, the Logo and some svg structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import * as React from "react";
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import { bright } from "./styled/theme";
+import Logo from "./components/Logo";
+let selectedTheme = bright;
 
 const AppWrapper = styled.div`
   height: 100vh;
@@ -11,8 +14,9 @@ const AppWrapper = styled.div`
     "roll-journal roll-journal"
     "dice-buttons dice-buttons";
 
+  font-family: "Dosis", sans-serif;
+
   div {
-    border: 1px solid black;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -37,31 +41,26 @@ const AppWrapper = styled.div`
   }
 `;
 
-const Logo = styled.div`
-  grid-area: logo;
-  background-color: rgba(255, 0, 0, 0.05);
-`;
 const TopControls = styled.div`
   grid-area: top-controls;
-  background-color: rgba(255, 255, 0, 0.05);
 `;
 const DiceButtons = styled.div`
   grid-area: dice-buttons;
-  background-color: rgba(255, 0, 255, 0.05);
 `;
 const RollJournal = styled.div`
   grid-area: roll-journal;
-  background-color: rgba(0, 255, 255, 0.05);
 `;
 
 const BeDiceApp: React.FC = () => {
   return (
-    <AppWrapper>
-      <Logo>Logo</Logo>
-      <TopControls>Top Controls</TopControls>
-      <DiceButtons>Dice Icons</DiceButtons>
-      <RollJournal>Roll Journal And Members</RollJournal>
-    </AppWrapper>
+    <ThemeProvider theme={selectedTheme}>
+      <AppWrapper>
+        <Logo />
+        <TopControls>Top Controls</TopControls>
+        <DiceButtons>Dice Icons</DiceButtons>
+        <RollJournal>Roll Journal And Members</RollJournal>
+      </AppWrapper>
+    </ThemeProvider>
   );
 };
 
---
+++ src/styled/theme.ts
@@ -0,0 +1,24 @@
+interface Theme {
+  main: string,
+  secondary: string,
+  bg: string,
+  headerBg: string,
+  logoColor: string
+}
+
+export const dark: Theme = {
+  main: "grey",
+  secondary: "orange",
+  bg: `linear-gradient(to right, rgb(248, 207, 247), rgb(212, 251, 254));`,
+  headerBg: "#000",
+  logoColor: "white"
+};
+
+export const bright: Theme = {
+  main: "white",
+  secondary: "yellow",
+  bg: `linear-gradient(to right, rgb(248, 207, 247), rgb(212, 251, 254));`,
+  headerBg: "#fff",
+  logoColor: "#00D0AE"
+};
+
