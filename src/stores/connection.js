4e282b53a814271e4ae92859d7b5e58fd86caa98
Slava Bezgachev <bezgachev@gmail.com>
2019-09-22 10:15:41 +0600

    Upgraded deps, fixed build errors

--- src/stores/connection.js
+++ src/stores/connection.js
@@ -1,7 +1,7 @@
 import { createContext } from "react";
 import io from "socket.io-client";
 
-const API_URL = "";
+const API_URL = "https://dev.api.be-dice.com";
 
 const initialState = {
   socket: null,
